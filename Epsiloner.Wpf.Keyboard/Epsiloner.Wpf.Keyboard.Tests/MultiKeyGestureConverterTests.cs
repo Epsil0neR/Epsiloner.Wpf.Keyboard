2e524476d71855282126ecd63caa36af6c439f7f
Epsil0neR <epsil0neris@gmail.com>
2018-08-12 18:00:25 +0300

    [TEST] Added tests for MultiKeyGestureSerializer and MultiKeyGestureConverter

---
+++ Epsiloner.Wpf.Keyboard/Epsiloner.Wpf.Keyboard.Tests/MultiKeyGestureConverterTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Epsiloner.Wpf.Keyboard.Tests
+{
+    [TestClass()]
+    public class MultiKeyGestureConverterTests
+    {
+        [TestMethod()]
+        public void MultiKeyGestureConverter_constructor()
+        {
+            var c = new MultiKeyGestureConverter();
+            Assert.IsNotNull(c);
+        }
+
+        [DataRow(true, typeof(string))]
+        [DataRow(false, typeof(int))]
+        [DataRow(false, typeof(Gesture))]
+        [DataRow(false, typeof(MultiKeyGesture))]
+        [TestMethod()]
+        public void CanConvertFromTest(bool expected, Type type)
+        {
+            var c = new MultiKeyGestureConverter();
+            Assert.AreEqual(expected, c.CanConvertFrom(null, type));
+        }
+
+        [DataRow("a control+b", "a ctrl+b")]
+        [DataRow("a control+b", "a control+b")]
+        [DataRow("a alt+b c", "a alt+b c")]
+        [DataRow("a windows+b", "a win+b")]
+        [DataRow("a windows+b", "a windows+b")]
+        [DataRow("control t e s t", "ctrl t e s t")]
+        [DataRow("control t e s t", "control t e s t")]
+        [DataRow("t e s t", "t e s t")]
+        [TestMethod()]
+        public void ConvertFromTest(string expected, string input)
+        {
+            var c = new MultiKeyGestureConverter();
+            var s = new MultiKeyGestureSerializer();
+
+            var o = c.ConvertFrom(input);
+            Assert.IsNotNull(o);
+            Assert.IsInstanceOfType(o, typeof(MultiKeyGesture));
+            var g = o as MultiKeyGesture;
+            var t = s.ConvertToString(g, null);
+
+            Assert.AreEqual(expected.ToLowerInvariant(), t.ToLowerInvariant());
+        }
+    }
+}
---
+++ Epsiloner.Wpf.Keyboard/Epsiloner.Wpf.Keyboard.Tests/MultiKeyGestureSerializerTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Input;
+
+namespace Epsiloner.Wpf.Keyboard.Tests
+{
+    [TestClass()]
+    public class MultiKeyGestureSerializerTests
+    {
+        [TestMethod()]
+        public void CanConvertToStringTest()
+        {
+            var s = new MultiKeyGestureSerializer();
+            var g = new MultiKeyGesture(new[]
+            {
+                new Gesture(Key.None, ModifierKeys.Control),
+                new Gesture(Key.T),
+                new Gesture(Key.E),
+                new Gesture(Key.S),
+                new Gesture(Key.T),
+            });
+
+            Assert.IsTrue(s.CanConvertToString(g, null));
+        }
+
+        [TestMethod()]
+        public void ConvertToStringTest()
+        {
+            var s = new MultiKeyGestureSerializer();
+            var g = new MultiKeyGesture(new[]
+            {
+                new Gesture(Key.None, ModifierKeys.Control),
+                new Gesture(Key.T),
+                new Gesture(Key.E),
+                new Gesture(Key.S),
+                new Gesture(Key.T),
+            });
+            var res = s.ConvertToString(g, null);
+            Assert.AreEqual("Control T E S T".ToLowerInvariant(), res.ToLowerInvariant());
+        }
+    }
+}
