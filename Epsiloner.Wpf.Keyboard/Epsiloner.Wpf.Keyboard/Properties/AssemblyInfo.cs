d54ccaf5678d14967684e1289e8748fe8ef511c5
Epsil0neR <epsil0neris@gmail.com>
2018-11-14 15:47:11 +0200

    v1.0.4 Moved to new project format, added support for .NET v4.5 - 4.7

--- Epsiloner.Wpf.Keyboard/Epsiloner.Wpf.Keyboard/Properties/AssemblyInfo.cs
+++ Epsiloner.Wpf.Keyboard/Epsiloner.Wpf.Keyboard/Properties/AssemblyInfo.cs
@@ -1,18 +1,5 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Windows.Markup;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Epsiloner.Wpf.Keyboard")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("https://github.com/Epsil0neR/Epsiloner.Wpf.Keyboard")]
-[assembly: AssemblyProduct("Epsiloner.Wpf.Keyboard")]
-[assembly: AssemblyCopyright("Copyright © Epsil0neR 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -21,17 +8,4 @@ using System.Windows.Markup;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("33035da6-3daf-49e6-b3f7-b96ee569688b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
 [assembly: XmlnsDefinition("http://epsiloner/wpf/keyboard/keybinding", "Epsiloner.Wpf.Keyboard.KeyBinding")]
